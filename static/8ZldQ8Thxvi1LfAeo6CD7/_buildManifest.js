fad4de9911b0e967df67d78869e42d1d60f79c8e
migueldevacv <mikeangel7879@gmail.coom>
2024-06-25 20:29:29 -0600

    first commit

---
+++ static/8ZldQ8Thxvi1LfAeo6CD7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/929-55ec6cf041b8b8be.js","static/chunks/pages/index-a0acac9a7c4d403e.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
